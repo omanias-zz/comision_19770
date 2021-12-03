8a50502a4189b7fd383163c326158198ba09fe4f
Horacio Gutierrez <horacio.estevez@gmail.com>
2021-12-02 22:41:51 -0300

    Clase 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Header from "./Header"
 
 function App () {
+
+    //const links = ["link1","link2"]
+    const links=[ 
+        {href:"#", name:"inicio",id:1}, 
+        {href:"#", name:"Productos",id:2}, 
+        {href:"#", name:"Contactos",id:3}, 
+        {href:"#", name:"Carrito",id:4} 
+    ]
+
     return (
         <>
-            <Header/>
+            <Header nombre={"Horacio"} edad={1} links={links}/>
             <main>soy main</main>
         </>
     )
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
-const Header = () => {
+const Header = (props) => {
+
+    console.log(props)
+    console.log(props.nombre)
+
+
     return (
         <header id="main-header" className="header">
-            <h1>E-Commerce</h1>
+            <h1>Bienvenido {props.nombre}!</h1>
             <span className="material-icons">
                 shopping_cart
             </span>
             <img src="/logo192.png" alt="logo" />
             <nav>
-                <a href="#">link</a>
-                <a href="#">link</a>
+                {props.links.map((elemento, indice) => {
+                    return <a key={elemento.id} href={elemento.href}>{elemento.name}</a>
+                })}
             </nav>
         </header>
     )
