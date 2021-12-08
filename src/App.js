6fc046d9c65f6570ba5d6b6aef64493df9ac5ac5
Horacio Gutierrez <horacio.estevez@gmail.com>
2021-12-07 22:34:39 -0300

    Estados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
+import { useState } from 'react'
 import Header from "./Header"
 import ItemListContainer from "./ItemListContainer"
 import "./app.scss"
 
 
-function App () {
 
-    //const links = ["link1","link2"]
-    const links=[ 
-        {href:"#", name:"inicio",id:1}, 
-        {href:"#", name:"Productos",id:2}, 
-        {href:"#", name:"Contactos",id:3}, 
-        {href:"#", name:"Carrito",id:4} 
+
+function App() {
+    
+    /* const resultado = useState("")
+    const estado = resultado[0]
+    const setEstado = resultado[1] */
+
+    let [contador, setContador] = useState(0)
+
+    let [estado,setEstado] = useState(false)
+
+    let [lista, setLista] = useState([])
+    
+
+    const links = [
+        { href: "#", name: "inicio", id: 1 },
+        { href: "#", name: "Productos", id: 2 },
+        { href: "#", name: "Contactos", id: 3 },
+        { href: "#", name: "Carrito", id: 4 }
     ]
 
+    const foo = () => {
+        console.log("soy foo")
+    }
+
+    const sumar = () => {
+        //estado = true
+        //contador = 1
+        //contador++
+        setContador(contador + 1)
+
+        //setEstado(!estado)
+
+        //operador rest/spread
+        //slice()
+
+        //const copia = lista
+
+        setLista()
+    }
+
+
+    const restar = () => {
+        setContador(contador - 1)
+    }
+
+
+    const resetear = () => {
+        setContador(0)
+    }
+    
     return (
         <>
-            <Header nombre={"Horacio"} edad={1} links={links} />
+            <Header nombre={"E-Commerce"} edad={1} links={links} foo={foo}>
+                <p>Hola</p>
+                <p>Hola</p>
+            </Header>
             <main>soy main</main>
-            <ItemListContainer/>
+            <p>Contador Actual : {contador}</p>
+            <button onClick={sumar}>sumar</button>
+            <button onClick={resetear}>resetear</button>
+            <button onClick={restar}>restar</button>
+            <ItemListContainer />
         </>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,17 @@
-const Header = ({nombre,links}) => {
-
-    //console.log(props)
-    //console.log(props.nombre)
-
-    //const {nombre,links} = props
+const Header = ({nombre,links,foo,children}) => {
 
     return (
         <header id="main-header" className="header">
             <div id="brand">
                 <img src="/logo192.png" alt="logo" />
-                <h1>Bienvenido {nombre}!</h1>
+                <h1>{nombre}</h1>
             </div>
+            {/* {children} */}
             <nav>
                 {links.map((elemento, indice) => {
                     return <a key={elemento.id} href={elemento.href}>{elemento.name}</a>
                 })}
-                <span className="material-icons">
+                <span onClick={foo} className="material-icons">
                     shopping_cart
                 </span>
             </nav>
